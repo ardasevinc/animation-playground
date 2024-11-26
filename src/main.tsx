4e57b8aa652ea71408b0ddf36b9c14669819b0b7
Arda Sevinç <arda@ardasevinc.com>
2024-11-26 19:43:05 +0300

    feat: try changing root div id to fix route not rendering error

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ declare module '@tanstack/react-router' {
   }
 }
 
-const rootElement = document.getElementById('root')!;
+const rootElement = document.getElementById('app')!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
