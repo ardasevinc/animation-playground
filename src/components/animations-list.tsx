2e0e6eb4826a9742e0fb7d9c3edcb3ec92e31367
Arda Sevinç <arda@ardasevinc.com>
2024-11-26 19:16:09 +0300

    feat: add typing animation card

--- src/components/animations-list.tsx
+++ src/components/animations-list.tsx
@@ -1,19 +1,30 @@
 import { Link } from '@tanstack/react-router';
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 
-const animations = [{ href: '/number-animations', label: 'Number Animations' }];
+const animations = [
+  {
+    href: '/number-animations',
+    label: 'Number Animations',
+    description: 'Number animation go brrr...',
+  },
+  {
+    href: '/typing-animations',
+    label: 'Typing Animations',
+    description: 'All the typing animations',
+  },
+];
 
 const AnimationsList = () => {
   return (
-    <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+    <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-x-3'>
       {animations.map((animation) => (
         <li key={animation.href}>
           <Link to={animation.href}>
-            <Card>
+            <Card className='hover:border hover:border-slate-600 transition-all duration-300'>
               <CardHeader>
                 <CardTitle className='text-xl'>{animation.label}</CardTitle>
               </CardHeader>
-              <CardContent>Number animation go brrr...</CardContent>
+              <CardContent>{animation.description}</CardContent>
             </Card>
           </Link>
         </li>
