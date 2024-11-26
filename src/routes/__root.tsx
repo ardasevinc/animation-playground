2c121aea2dac0de3e9cf3e0d22cbdba4d8d3526c
Arda Sevinç <arda@ardasevinc.com>
2024-11-26 19:43:43 +0300

    Revert "feat: try changing root div id to fix route not rendering error"
    This reverts commit 4e57b8aa652ea71408b0ddf36b9c14669819b0b7.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,38 @@
-import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@/components/tanstack-router-devtools';
+import { Badge } from '@/components/ui/badge';
+import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { Suspense } from 'react';
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
+  notFoundComponent: NotFound,
+});
+
+function NotFound() {
+  return (
+    <main className='flex-1 flex'>
+      <section className='flex flex-col items-center'>
+        <Badge variant='outline'>404</Badge>
+        <h2>Not Found</h2>
+      </section>
+    </main>
+  );
+}
 
 function RootComponent() {
   return (
-    <React.Fragment>
-      <div>Hello "__root"!</div>
-      <Outlet />
-    </React.Fragment>
-  )
+    <>
+      <div className='min-h-screen flex flex-col px-2 lg:px-4 2xl:px-10 gap-y-4 lg:gap-y-10'>
+        <header className='w-full'>
+          <h1 className='text-3xl text-center pt-2 lg:pt-6'>
+            Animation Playground
+          </h1>
+        </header>
+        <Outlet />
+      </div>
+      <Suspense>
+        <TanStackRouterDevtools position='bottom-right' />
+      </Suspense>
+    </>
+  );
 }
